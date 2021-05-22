335b5316b5027bf7f0993cf858a8f8147c5bc1bc
mflilian <mayhhara@hotmail.com.br>
2021-05-21 21:49:54 -0300

    adds symbols example

---
+++ symbols.js
@@ -0,0 +1,8 @@
+/**
+  * A função Symbol() retorna um valor do tipo símbolo (symbol), tem propriedades estáticas que expõem vários membros dos objetos nativos,
+  * possuem métodos estáticos que expõem o registro de símbolos globais e se parecem com uma classe de objeto nativo,
+  * mas estão incompletos como construtor porque não suportam a sintaxe "new Symbol()" .
+  */
+
+ var pa = Symbol();
+ var bar = Symbol("pa");
